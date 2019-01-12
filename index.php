97b29c751f7711556c31a36b872f59fac973a4a1
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-12 21:55:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/Rubbermaid-Brilliance-Container-14-Piece-1977447/dp/B01JCNETC0/ref=lp_18505439011_1_14?srs=18505439011&ie=UTF8&qid=1547310270&sr=8-14">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/Rubbermaid-Brilliance-Container-14-Piece-1977447/dp/B01JCNETC0/ref=lp_18505439011_1_14?srs=18505439011&ie=UTF8&qid=1547310270&sr=8-14"
         </script>
         <title>Page Redirection</title>
     </head>
